8a7f4d46b4f5681e1af6a2250d52fe062436931c
Daniel Earl <danielearl@daniels-mac-mini.local>
2018-11-22 16:42:58 +0000

    Initial Commit

---
+++ EFL Grounds/LeagueOneTableViewController.swift
@@ -0,0 +1,101 @@
+//
+//  LeagueOneTableViewController.swift
+//  EFL Grounds
+//
+//  Created by Macbook Pro on 15/08/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class LeagueOneTableViewController: UITableViewController {
+
+    var Teams: [String] = ["Accrington Stanley","AFC Wimbledon","Barnsley","Blackpool","Bradford City","Bristol Rovers","Burton Albion","Charlton Athletic","Coventry","Doncaster Rovers","Fleetwood","Gillingham","Luton Town","Oxford Utd","Peterborough Utd","Plymouth","Portsmouth","Rochdale","Scunthorpe Utd","Shrewsbury Town","Southend Utd","Sunderland","Walsall","Wycombe Wanderers"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Teams.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "oneTeamCell", for: indexPath)
+        
+        // Configure the cell...
+        cell.textLabel?.text = Teams[indexPath.row]
+        let teamIcon: UIImage=UIImage(named: cell.textLabel!.text!)!
+        cell.imageView!.image=teamIcon
+        return cell
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        super.prepare(for: segue, sender: sender)
+        let indexPath = tableView.indexPathForSelectedRow!
+        let selected = indexPath.row
+        let teamDetailViewController = segue.destination as? TeamViewController
+        let team = Teams[selected]
+        teamDetailViewController?.teamName = team
+        teamDetailViewController?.league = "one"
+    }
+}
---
+++ EFL Grounds/TeamFixturesViewController.swift
@@ -0,0 +1,72 @@
+//
+//  TeamFixturesViewController.swift
+//  EFL Grounds
+//
+//  Created by Macbook Pro on 15/08/2018.
+//  Copyright © 2018 Macbook Pro. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class TeamFixturesViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loadingText: UILabel!
+    @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
+    
+    func loadFixturesData() {
+        loadingText.text = "Loading Fixtures"
+        let fixturesURL = (parent as! TeamViewController).teamFixtures
+        let myURL = URL(string: fixturesURL)
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        webView.navigationDelegate = self
+        loadFixturesData()
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        loadingText.isHidden = true
+        loadingSpinner.isHidden = true
+    }
+    
+    func webView(_ webView: WKWebView,
+                 didFailProvisionalNavigation navigation: WKNavigation!,
+                 withError error: Error) {
+        if(error._code == NSURLErrorNotConnectedToInternet) {
+            loadingSpinner.isHidden = true
+            loadingText.text = "ERROR: No Internet Connction"
+        }
+        print("Error Loading:")
+        print(error._code)
+    }
+        
+    func webView(_ webView: WKWebView,
+                 didFail navigation: WKNavigation!,
+                 withError error: Error) {
+        print("Error Loading: Navigation Error")
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
