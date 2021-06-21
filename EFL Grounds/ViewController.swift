50a3b1dcecebfaf1811413195d2d4888d3bb7abe
Daniel Earl <danielearl@me.com>
2021-06-21 18:13:41 +0100

    Add App Store Review Prompt

--- EFL Grounds/ViewController.swift
+++ EFL Grounds/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
@@ -14,9 +15,33 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showAppReviewPopover()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: App Store Review code
+    func showAppReviewPopover() {
+        var count = UserDefaults.standard.integer(forKey: "userAppLoadCount")
+        count += 1
+        UserDefaults.standard.set(count, forKey: "userAppLoadCount")
+       
+        // Get the current bundle version for the app
+        let infoDictionaryKey = kCFBundleVersionKey as String
+        guard let currentVersion = Bundle.main.object(forInfoDictionaryKey: infoDictionaryKey) as? String
+            else { fatalError("Expected to find a bundle version in the info dictionary") }
+        
+        let lastVersionPromptedForReview = UserDefaults.standard.string(forKey: "lastVersionPromptedForReview")
+     
+        // Has the process been completed several times and the user has not already been prompted for this version?
+        if count >= 15 && currentVersion != lastVersionPromptedForReview {
+            SKStoreReviewController.requestReview()
+            UserDefaults.standard.set(currentVersion, forKey: "lastVersionPromptedForReview")
+        }
     }
 }
